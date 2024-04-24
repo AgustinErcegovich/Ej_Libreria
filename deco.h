b24143c08cff8581d33054c5bdd9170679304de8
AgustinErcegovich <166656494+AgustinErcegovich@users.noreply.github.com>
2024-04-24 14:40:50 -0300

    deco.h

--- deco.h
+++ deco.h
@@ -1,10 +1,3 @@
-/* Sintetizando:
-a) Deberán crear una librería en la que contenga las funciones necesarias
-para el control independiente de cada led. Cada funcion de encendido o 
-apagado, deberá funcionar de la siguiente manera: Si dentro de la funcion 
-se encuentra un ‘1’ se debera prender el led correspondiente, mientras que 
-si hay un ‘0’ se deberá apagar. Se ingresará por función el pin y el estado.*/
-
 #ifndef DECO_H
 #define DECO_H
 
